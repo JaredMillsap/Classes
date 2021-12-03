56562e071537c51ec6826d469205b59b3d31e69c
JaredMillsap <jmillsap23@gmail.com>
2021-12-03 10:26:08 -0600

    new

---
+++ Classes/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car1 = new Car("Ford","Explorer",2010 );
+
+            Car car2 = new Car("Chevy", "Silverado", 2012);
+
+            Car car3 = new Car("Toyta", "Camry", 2015);
+
+            Console.WriteLine(car1);
+            Console.WriteLine(car2);
+            Console.WriteLine(car3);
+        }
+    }
+}
